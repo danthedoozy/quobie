563952e22eed7c42a4e3475335347a5bcb229564
Daniel Driskill <danieldriskill@Daniels-MacBook-Pro-2.local>
2019-01-21 20:10:31 -0700

    Add quote spacing and flex display.

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import Quote from './Quote';
 
-const Quotes = ({ quotes }) => quotes.map(quote => <Quote quote={quote} />);
+const Quotes = ({ quotes }) => (
+  <div className="quotes">
+    {quotes.map(quote => <Quote quote={quote} />)}
+  </div>
+);
 
 export default Quotes;
