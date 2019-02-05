a739a8bd2bea8b943f91e22b764a28a69f341686
Daniel Driskill <daniel.driskill@endurance.com>
2019-02-05 12:28:48 -0700

    Add report and clap methods (unfinished) to App.js.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import AddQuote from './AddQuote';
 import Discussion from './Discussion';
 import Footer from './Footer';
 import quotes from '../services/mock';
-import '../assets/styles/App.css';
+import '../assets/styles/components/App.css';
 
 class App extends Component {
   // This state is hypothetical (we will likely add some state management)
@@ -37,6 +37,30 @@ class App extends Component {
     // API call that adds a comment to the database -- console log data for now
     console.log(newComment);
   }
+
+  // Clap for quote
+  quoteClap = (quoteId, userId) => {
+    // API call that adds a clap to a quote
+    console.log(`${userId} has clapped for quote ${quoteId}`);
+  };
+
+  // Clap for comment
+  commentClap = (commentId, userId) => {
+    // API call that adds a clap to a comment
+    console.log(`${userId} has clapped for comment ${commentId}`);
+  }
+
+  // Report quote
+  reportQuote = (quoteId, userId) => {
+    // API call that adds a flag to a quote
+    console.log(`${userId} has reported quote ${quoteId}`);
+  };
+
+  // Report comment
+  reportComment = (commentId, userId) => {
+    // API call that adds a flag to a comment
+    console.log(`${userId} has reported comment ${commentId}`);
+  };
 
   render() {
     const { addQuote, addComment } = this;
